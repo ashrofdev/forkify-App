08ee9ecf5b59b6ddfc65863abb134946bb365c0e
Ashrof Salman <salmanashraf853@gmail.com>
2019-06-17 13:00:29 -0700

    a

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+//d5bb331a6b84d053e3b6e2e8c1cec561
+//5a9f62241058604119173d56902e498e
+export const proxy = 'https://cors-anywhere.herokuapp.com/'
+export const key = '5a9f62241058604119173d56902e498e'
